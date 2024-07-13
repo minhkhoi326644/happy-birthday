b5a9f8bf0ef2082ce990e76ca6b337144a773e6d
Minh Khoi <tranleminhkhoi1510@gmail.com>
2024-07-13 16:27:09 +0700

    hg

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật Em Bé của anh';
+const titleCard = 'Tặng em bé';
+const contentCard = 'Chúc em bé của anh sinh nhật vui vẻ, tuổi 17 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Mong là em sẽ luôn bên cạnh và đồng hành cùng anh trên hành trình trưởng thành <3';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
